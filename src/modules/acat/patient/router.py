fbaa3739df27fed9a2683089221b564f118a20bd
Dr.Kenobi27 <josgarber6@alum.us.es>
2024-02-22 21:27:08 +0100

    feat: Added patients list #3 Patients list added succesfully

--- src/modules/acat/patient/router.py
+++ src/modules/acat/patient/router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from src.modules.acat.patient import controller
+from src.core.deps import SessionDep
+from typing import Any
 
 router = APIRouter()
 
@@ -6,3 +9,8 @@ router = APIRouter()
 @router.get('/')
 def root():
     return 'Hello acat patient router!'
+
+@router.get("/list")
+async def get_patients(session: SessionDep) -> Any:
+    """Get all patients from the database."""
+    return await controller.get_patients_controller(session)
