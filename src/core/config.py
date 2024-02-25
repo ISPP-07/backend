9fe3e7d28b2bc6c39cef759db6121cf8577f4142
gonzcm <gcamposmejias@gmail.com>
2024-02-25 12:36:07 +0100

    conf: Updated deprecated framework config

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import secrets
 from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl, PostgresDsn, validator
-from typing import Any, Dict, List, Optional, Union
+from pydantic import AnyHttpUrl, PostgresDsn, ValidationInfo, field_validator
+from typing import Any, List, Optional, Union
 from fastapi.responses import JSONResponse
 
 APP_DIR = Path(__file__).resolve().parent
@@ -24,7 +24,8 @@ class Settings(BaseSettings):
 
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    @classmethod
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
@@ -39,17 +40,18 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], values: ValidationInfo) -> Any:
         if isinstance(v, str):
             return v
         return PostgresDsn.build(
             scheme="postgresql+psycopg",
-            username=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_HOST"),
-            port=values.get("POSTGRES_PORT"),
-            path=values.get("POSTGRES_DB") or ""
+            username=values.data.get("POSTGRES_USER"),
+            password=values.data.get("POSTGRES_PASSWORD"),
+            host=values.data.get("POSTGRES_HOST"),
+            port=values.data.get("POSTGRES_PORT"),
+            path=values.data.get("POSTGRES_DB") or ""
         )
 
     @property
