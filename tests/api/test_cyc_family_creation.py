d14c90980cdf587e2a3037cff759030f5cbc3e03
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-02-27 19:19:49 +0100

    fix: Changed family response status #6
    The response status returned when creating a family has been changed to 201. The test has also been fixed.

--- tests/api/test_cyc_family_creation.py
+++ tests/api/test_cyc_family_creation.py
@@ -1,5 +1,4 @@
 import pytest
-from src.modules.acat.patient.model import Patient
 from fastapi.testclient import TestClient
 from src.core.config import settings
 
@@ -9,19 +8,18 @@ async def test_create_family(client: TestClient):
     url = f'{settings.API_STR}cyc/family/'
 
     family_data = {
-        "id": 1,
         "name": "La Familia",
         "phone": "123456789",
         "address": "123 Main St",
         "number_of_people": 4,
         "referred_organization": "Hospital ABC",
-        "next_renewal_date": "2024-02-26",
+        "next_renewal_date": "2100-12-31",
         "derecognition_state": "Active"
     }
 
     response = client.post(url, json=family_data)
 
-    assert response.status_code == 200
+    assert response.status_code == 201
     response_data = response.json()
     assert response_data["name"] == family_data["name"]
     assert response_data["address"] == family_data["address"]
