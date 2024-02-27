1bec260bf24e35e8d66d3efe30508ad9a5f9c230
Joaquin Martin <jmartinacu2002@gmail.com>
2024-02-27 11:52:44 +0100

    feat: Added product and warehouse models #40
    Added product and warehouse models to family models and changed age rank
    functionality in CC organization.

--- src/modules/cyc/family/model.py
+++ src/modules/cyc/family/model.py
@@ -1,9 +1,36 @@
-from typing import Optional
+from sys import maxsize
+from typing import Optional, Dict, Literal
 from enum import Enum
 from datetime import date
 from sqlmodel import Field, Relationship
+from pydantic import PositiveInt, PastDatetime, FutureDatetime
 
 from src.core.database.base_crud import Base
+
+AGE_RANGES: Dict[
+    Literal['baby', 'child', 'adult', 'senior', 'old'],
+    Dict[Literal['min', 'max'], int]
+] = {
+    'baby': {'min': 0, 'max': 3, },
+    'child': {'min': 3, 'max': 15, },
+    'adult': {'min': 15, 'max': 45, },
+    'senior': {'min': 45, 'max': 60, },
+    'old': {'min': 60, 'max': maxsize, },
+}
+
+
+def calculate_age(birth_date: date) -> int:
+    today = date.today()
+    return today.year - birth_date.year - (
+        (today.month, today.day) < (birth_date.month, birth_date.day)
+    )
+
+
+def get_age_rank(age: int) -> str:
+    for key, entry in AGE_RANGES.items():
+        if entry.get('min') < age <= entry.get('max'):
+            return key
+    raise ValueError('Age cannot be negative')
 
 
 class DerecognitionStatus(str, Enum):
@@ -11,27 +38,29 @@ class DerecognitionStatus(str, Enum):
     SUSPENDED = 'Suspended'
 
 
-class AgeRange(Base, table=True):
+class Person(Base):
+    date_birth: PastDatetime
+    family_id: Optional[int] = Field(default=None, foreign_key='family.id')
+    family: 'Family' = Relationship(back_populates='persons')
+
+    def age(self) -> int:
+        return calculate_age(self.date_birth)
+
+    def age_rank(self) -> str:
+        return get_age_rank(self.age)
+
+
+class Adult(Person, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    MinAge: int
-    MaxAge: int
+    name: str
+    dni: str
 
 
-class FamilyAgeRangeLink(Base, table=True):
-    family_id: Optional[int] = Field(
-        default=None,
-        foreign_key='family.id',
-        primary_key=True,
-    )
-    age_range_id: Optional[int] = Field(
-        default=None,
-        foreign_key='agerange.id',
-        primary_key=True,
-    )
+class Child(Person, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
 
 
 # This class is commented because the implementation of its functionalities will not be done in this sprint.
-
 # class DeliveryHistory(Base, table=True):
 #     id: Optional[int] = Field(default=None, primary_key=True)
 #     delivery_date: date
@@ -42,7 +71,26 @@ class FamilyObservation(Base, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     observation_text: str
     family_id: Optional[int] = Field(default=None, foreign_key='family.id')
-    family: Optional['Family'] = Relationship(back_populates='observations')
+    family: 'Family' = Relationship(back_populates='observations')
+
+
+# POR AHORA LO HE RELACIONADO COMO 1-* PERO PODRIA SER *-*
+# SI UN MISMO PRODUCTO LO GUARDAN EN ALMACENES DIFERENTES
+class Product(Base, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    quantity: PositiveInt
+    exp_date: Optional[FutureDatetime]
+    warehouse_id: Optional[int] = Field(
+        default=None, foreign_key='warehouse.id'
+    )
+    warehouse: 'Warehouse' = Relationship(back_populates='products')
+
+
+class Warehouse(Base, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    products: list[Product] = Relationship(back_populates='warehouse')
 
 
 class Family(Base, table=True):
@@ -50,14 +98,14 @@ class Family(Base, table=True):
     name: str
     phone: str
     address: str
-    age_ranges: list[AgeRange] = Relationship(link_model=FamilyAgeRangeLink)
     number_of_people: int
     referred_organization: str
-    next_renewal_date: date
+    next_renewal_date: FutureDatetime
     derecognition_state: DerecognitionStatus
     observations: list[FamilyObservation] = Relationship(
         back_populates='family'
     )
+    persons: list[Person] = Relationship(back_populates='family')
     # delivery_history: list[DeliveryHistory] = Relationship(
     #     back_populates='family_id',
     # )
