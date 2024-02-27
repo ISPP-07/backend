dff10bae345dd01c74318fae655703ee4e857bde
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-02-27 19:19:49 +0100

    fix: Changed family response status #6
    The response status returned when creating a family has been changed to 201. The test has also been fixed.

--- src/modules/cyc/family/router.py
+++ src/modules/cyc/family/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from src.core.deps import SessionDep
 from src.modules.cyc.family.controller import create_family_controller
 from src.modules.cyc.family.model import Family
+from fastapi import status
 
 router = APIRouter()
 
@@ -12,6 +13,6 @@ def root():
     return 'Hello cyc family router!'
 
 
-@router.post('/')
+@router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_family(session: SessionDep, family: Family) -> Any:
     return await create_family_controller(session, family)
