af8eff3be19ea5dd98a9f150fb1d676e153a6a48
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-02-29 21:24:19 +0100

    fix: Removed trailing whitespace

--- src/modules/cyc/family/service.py
+++ src/modules/cyc/family/service.py
@@ -3,6 +3,7 @@ from src.modules.cyc.family.model import Family
 
 async def get_families_service(session):
     return await Family.get_multi(session)
-  
+
+
 async def create_family_service(session, family: Family):
     return await Family.create(session, **family.model_dump())
