de3b159d56c9669d66c6cdff98c969cf3adc8990
davidg43 <david.gavira.43@gmail.com>
2024-03-04 10:26:40 +0100

    test: Changes requested v2

--- tests/api/test_cyc_family_details.py
+++ tests/api/test_cyc_family_details.py
@@ -1,26 +1,32 @@
-import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
 
 from src.core.config import settings
 
-from src.modules.cyc.family.model import Family
+from src.modules.cyc.family.model import Family, Person
 
 
-@pytest.mark.asyncio
+@pytest_asyncio.fixture
 async def create_family(session) -> Family:
     person1_data = {
         "date_birth": "2005-01-01",
         "type": "Child",
         "name": "Jane Doe",
-        "dni": "87654321"
+        "dni": "87654321",
+        "family_id": "1"
     }
 
     person2_data = {
         "date_birth": "2000-01-01",
         "type": "Adult",
         "name": "John Doe",
-        "dni": "12345678"
+        "dni": "12345678",
+        "family_id": "1"
     }
+
+    await Person.create(session, **person1_data)
+    await Person.create(session, **person2_data)
+
     family_data = {
         "id": 1,
         "name": "La Familia",
@@ -31,20 +37,16 @@ async def create_family(session) -> Family:
         "next_renewal_date": "2100-12-31",
         "derecognition_state": "Active",
         "obsarvations": "Observation 1",
-        "members": [
-            person1_data,
-            person2_data
-        ]
     }
     family = await Family.create(session, **family_data)
+
     return family
 
 
 def test_get_family_details(client: TestClient, create_family: Family):
-    family_id = create_family["id"]
-    url = (f'{settings.API_STR}'
-           f'cyc/family/' + str(family_id))
+    family_id = create_family.id
+    url = (f'{settings.API_STR}cyc/family/'  + str(family_id))
 
     response = client.get(url)
     assert response.status_code == 200
-    assert response.json() == create_family.model_dump()
+    assert response.json()["id"] == family_id
