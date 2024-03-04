b051b8fd3c5a75f39958cbe3c7355d4fecaae0e9
gonzcm <gcamposmejias@gmail.com>
2024-03-04 13:40:20 +0100

    fix: Fixed integration errors

--- src/modules/acat/patient/service.py
+++ src/modules/acat/patient/service.py
@@ -17,45 +17,5 @@ async def get_patients_service(session):
 
 
 async def get_patient_details_service(session, patient_id: int):
-    appointment_subq = (
-        select(
-            Appointment.patient_id,
-            Appointment.technician_id,
-            func.row_number().over(
-                partition_by=Appointment.patient_id,
-                order_by=Appointment.appointment_date.asc()
-            ).label("rn")
-        )
-        .filter(Appointment.patient_id == patient_id)
-        .subquery()
-    )
-
-    first_technician = aliased(Technician)
-
-    # Modificar la consulta para seleccionar también los detalles del
-    # Technician y la observación del paciente
-    patient_query = (
-        select(
-            Patient,
-            first_technician.name.label("technician_name"),
-            PatientObservation.observation_text  # Selecciona el texto de la observación
-        )
-        .join(appointment_subq, Patient.id == appointment_subq.c.patient_id)
-        .join(first_technician, first_technician.id == appointment_subq.c.technician_id)
-        # Une la tabla de observaciones
-        .join(PatientObservation, PatientObservation.patient_id == Patient.id)
-        # Considera solo el primer nombramiento
-        .where(appointment_subq.c.rn == 1)
-        .options(joinedload(Patient.observations))  # Eager load observations
-        .filter(Patient.id == patient_id)
-    )
-
-    result = await session.execute(patient_query)
-    # Incluye observation_text en la desestructuración
-    patient, technician_name, observation_text = result.first()
-    result = patient.model_dump()
-    result["first_technician_name"] = technician_name
-    # Añade observation_text al resultado
-    result["observation_text"] = observation_text
-
-    return result
+   obj = await Patient.get(session, id=patient_id)
+   return obj
