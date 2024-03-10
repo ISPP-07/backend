6e227606d1a02cfd2bc832a512087979c3ded51c
josgarber6 <josgarber6@users.noreply.github.com>
2024-03-10 12:27:07 +0000

    refactor: Applied Autopep8 action corrections

--- src/modules/cyc/delivery/controller.py
+++ src/modules/cyc/delivery/controller.py
@@ -2,5 +2,6 @@
 from src.modules.cyc.delivery import service
 from src.core.deps import DataBaseDep
 
+
 async def get_deliveries_controller(db: DataBaseDep):
-  return await service.get_deliveries_service(db)
+    return await service.get_deliveries_service(db)
--- src/modules/cyc/delivery/router.py
+++ src/modules/cyc/delivery/router.py
@@ -6,7 +6,7 @@ from src.modules.cyc.delivery.model import Delivery
 
 router = APIRouter()
 
+
 @router.get("/", status_code=status.HTTP_200_OK, response_model=list[Delivery])
 async def get_deliveries(db: DataBaseDep) -> List[Delivery]:
     return await controller.get_deliveries_controller(db)
-
