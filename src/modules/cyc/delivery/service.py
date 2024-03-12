6017097bc370fecf3ca44d28a132f75f9ed6955d
davidg43 <david.gavira.43@gmail.com>
2024-03-12 12:53:03 +0100

    feat: Api call get details #82

--- src/modules/cyc/delivery/service.py
+++ src/modules/cyc/delivery/service.py
@@ -1,1 +1,6 @@
-# Delivery service
+from src.core.deps import DataBaseDep
+from src.modules.cyc.delivery.model import Delivery
+
+
+async def get_delivery_service(db: DataBaseDep, query: dict) -> Delivery | None:
+    return await Delivery.get(db, query)
