8ebf25f0cf2d91e340fc42af63252e3e9e4380b9
Joaquin Martin <jmartinacu2002@gmail.com>
2024-03-12 16:09:35 +0100

    test: Created tests for auth resource #104
    Created tests for auth recourse and changed refresh endpoint to get
    refresh_token thrown header parameter "Refresh-Token" instead of a
    string in the request's body.

--- src/modules/shared/auth/service.py
+++ src/modules/shared/auth/service.py
@@ -1,3 +1,6 @@
+from fastapi.security import OAuth2PasswordRequestForm
+
+from src.core.deps import DataBaseDep
 from src.core.utils.security import verify_password
 from src.modules.shared.user.model import User
 
@@ -6,8 +9,8 @@ def root_service():
     return 'Hello core auth router!'
 
 
-async def login_service(db, form_data) -> User | None:
-    user = await User.get(db, query={'username': form_data.username})
+async def login_service(db: DataBaseDep, form_data: OAuth2PasswordRequestForm) -> User | None:
+    user: User = await User.get(db, query={'username': form_data.username})
     if not (user and verify_password(form_data.password, user.password)):
         return None
     return user
