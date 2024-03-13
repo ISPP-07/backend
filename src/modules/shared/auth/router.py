39987d501631685ec2cb5a416526e1da2943f6da
Joaquin Martin <jmartinacu2002@gmail.com>
2024-03-13 10:01:26 +0100

    fix: Fixed codacy issues #104

--- src/modules/shared/auth/router.py
+++ src/modules/shared/auth/router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Header, Depends, status
+from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from src.core.deps import DataBaseDep, get_current_user
