ae5fe1011e27547eba9c342d15c95f16ab07dddf
Alejandro Gallardo Pelayo <73127793+Thealean28@users.noreply.github.com>
2024-03-15 13:34:35 +0100

    fix: Add refactor patient details test #102

--- tests/api/test_acat_patient.py
+++ tests/api/test_acat_patient.py
@@ -47,6 +47,50 @@ async def insert_patients_mongo(mongo_db: Database):
     return result
 
 
+def test_get_patient_details(mongo_db: Database, app_client: TestClient):
+    patients = [
+        {
+            "_id": uuid4(),
+            "dossier_number": "1",
+            "name": "Paciente 1",
+            "first_surname": "AAA",
+            "second_surname": "BBB",
+            "birth_date": "2023-02-28",
+            "address": "Calle 1",
+            "contact_phone": "123123123",
+            "alias": "Paciente 1 AAA BBB",
+            "nid": "12343456M",
+            "first_technician": "string",
+            "gender": "Man",
+            "observations": "string"
+        },
+        {
+            "_id": uuid4(),
+            "dossier_number": "2",
+            "name": "Paciente 2",
+            "first_surname": "AAA",
+            "second_surname": "BBB",
+            "birth_date": "2023-02-28",
+            "address": "Calle 1",
+            "contact_phone": "123123123",
+            "alias": "Paciente 1 AAA BBB",
+            "nid": "12343456M",
+            "first_technician": "string",
+            "gender": "Man",
+            "observations": "string"
+        },
+    ]
+    for patient in patients:
+        mongo_db['Patient'].insert_one(patient)
+
+    url_get = f'{settings.API_STR}acat/patient/{patients[0]["_id"]}'
+    response = app_client.get(url_get)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["name"] == patients[0]["name"]
+    assert response_data["alias"] == patients[0]["alias"]
+
+
 def test_create_patient(app_client: TestClient):
     url = f'{settings.API_STR}acat/patient/'
 
@@ -85,20 +129,3 @@ def test_create_patient(app_client: TestClient):
 #     assert isinstance(result, list)
 #     assert len(result) == 2
 #     assert result == [patient.model_dump() for patient in create_patients]
-
-
-# @pytest.mark.asyncio
-# async def test_get_patient_details(client: TestClient, create_patients:
-# list[Patient]):
-
-#     patient = create_patients.pop()
-
-#     url_get = f'{settings.API_STR}acat/patient/details/{patient.id}'
-
-#     response = client.get(url_get)
-
-#     assert response.status_code == 200
-
-#     response_data = response.json()
-#     assert response_data["name"] == patient.name
-#     assert response_data["alias"] == patient.alias
