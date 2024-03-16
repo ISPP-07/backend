8666f2b688972f2747e31b6afe843aa6a0e1d2f2
Alejandro Gallardo Pelayo <73127793+Thealean28@users.noreply.github.com>
2024-03-16 11:34:37 +0100

    fix: Fixed test get patient details

--- tests/api/test_acat_patient.py
+++ tests/api/test_acat_patient.py
@@ -49,48 +49,18 @@ async def insert_patients_mongo(mongo_db: Database):
     return result
 
 
-def test_get_patient_details(mongo_db: Database, app_client: TestClient):
-    patients = [
-        {
-            "_id": uuid4(),
-            "dossier_number": "1",
-            "name": "Paciente 1",
-            "first_surname": "AAA",
-            "second_surname": "BBB",
-            "birth_date": "2023-02-28",
-            "address": "Calle 1",
-            "contact_phone": "123123123",
-            "alias": "Paciente 1 AAA BBB",
-            "nid": "12343456M",
-            "first_technician": "string",
-            "gender": "Man",
-            "observations": "string"
-        },
-        {
-            "_id": uuid4(),
-            "dossier_number": "2",
-            "name": "Paciente 2",
-            "first_surname": "AAA",
-            "second_surname": "BBB",
-            "birth_date": "2023-02-28",
-            "address": "Calle 1",
-            "contact_phone": "123123123",
-            "alias": "Paciente 1 AAA BBB",
-            "nid": "12343456M",
-            "first_technician": "string",
-            "gender": "Man",
-            "observations": "string"
-        },
-    ]
-    for patient in patients:
-        mongo_db['Patient'].insert_one(patient)
+def test_get_patient_details(app_client: TestClient, insert_patients_mongo):
+    patient_id = str(insert_patients_mongo[0]["_id"])
 
-    url_get = f'{settings.API_STR}acat/patient/{patients[0]["_id"]}'
+    url_get = f'{settings.API_STR}acat/patient/{patient_id}'
     response = app_client.get(url_get)
     assert response.status_code == 200
     response_data = response.json()
-    assert response_data["name"] == patients[0]["name"]
-    assert response_data["alias"] == patients[0]["alias"]
+    assert response_data["dossier_number"] == str(
+        insert_patients_mongo[0]["dossier_number"])
+    assert response_data["name"] == str(insert_patients_mongo[0]["name"])
+    assert response_data["first_surname"] == str(
+        insert_patients_mongo[0]["first_surname"])
 
 
 def test_create_patient(app_client: TestClient):
