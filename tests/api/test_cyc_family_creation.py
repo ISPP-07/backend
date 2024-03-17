3f965a3209e75dc31a6727f9520b7f53c78c5fb8
alblopnov <a.lopezbenjumea@gmail.com>
2024-03-17 19:42:47 +0100

    fix: Fixed requested changes

--- tests/api/test_cyc_family_creation.py
+++ tests/api/test_cyc_family_creation.py
@@ -1,50 +1,8 @@
-from datetime import datetime
-from uuid import uuid4
-from pymongo.database import Database
-import pytest_asyncio
 from fastapi.testclient import TestClient
 from src.core.config import settings
 
 
-# Fixture para insertar una familia de ejemplo en MongoDB
-
-
-@pytest_asyncio.fixture
-async def insert_family_mongo(mongo_db: Database):
-    family = {
-        "id": uuid4(),
-        "name": "Los García",
-        "phone": "987654321",
-        "address": "Calle Falsa 123",
-        "referred_organization": "Org Referida",
-        "next_renewal_date": datetime(2025, 1, 1).isoformat(),
-        "derecognition_state": "Active",
-        "observation": "Ninguna observación",
-        "number_of_people": 4,
-        "informed": True,
-        "members": [
-            {
-                "date_birth": "2010-05-01",
-                "type": "Child",
-                "name": "Luis",
-                "surname": "García",
-                "nationality": "Española",
-                "nid": "12345678A",
-                "family_head": False,
-                "gender": "Man",
-                "functional_diversity": False,
-                "food_intolerances": [],
-                "homeless": False
-            }
-            # Puedes agregar más miembros según sea necesario
-        ]
-    }
-
-    mongo_db['Family'].insert_one(family)
-    yield family
-
-
-def test_create_family(app_client: TestClient, insert_family_mongo):
+def test_create_family(app_client: TestClient):
     url = f'{settings.API_STR}cyc/family/'
     family_data = {
         "name": "La Familia Pérez",
